0f78c60e86f7fbae02ebd38abaf495ba63e915f1
Maksim Samorukov <53444059+MaximSamorukov@users.noreply.github.com>
2021-01-29 17:19:31 +0300

    updated state according to the task

--- state.js
+++ state.js
@@ -1,5 +1,5 @@
 const height = 3;
 const width = 4;
-let activeElements = [1, 5, 8];
-let selectedColor = 'blue'; // 'blue'  'red'
+let activeElements = [1];
+let selectedColor = 'red'; // 'blue'  'red'
 let option = 'many'; // 'one' 'many'
